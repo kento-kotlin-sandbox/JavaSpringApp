b4e0fb550c5ec8da2b83d786bc2aeec88887027a
Kento75 <kento2github@gmail.com>
2018-12-05 20:49:05 +0900

    Add login view

---
+++ src/main/java/com/example/demo/trySpring/login/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.demo.trySpring.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class LoginController {
+	// ログイン画面のGET用コントローラー
+	@GetMapping("/login")
+	public String getLogin(Model model) {
+		// login.htmlに遷移
+		return "login/login";
+	}
+	
+	// ログイン画面のPOST用コントローラー
+	@PostMapping("/login")
+	public String postLogin(Model model) {
+		// login.htmlに遷移
+		return "/login/login";
+	}
+}
