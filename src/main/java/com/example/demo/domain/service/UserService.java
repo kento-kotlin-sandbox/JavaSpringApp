26b8e8a740d5a9bad1c48145077e9d010819a7f5
Kento75 <kento2github@gmail.com>
2018-12-09 15:22:19 +0900

    Add insert method

--- src/main/java/com/example/demo/domain/service/UserService.java
+++ src/main/java/com/example/demo/domain/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.model.User;
 import com.example.demo.domain.model.repository.UserDao;
 
 
@@ -10,4 +11,20 @@ import com.example.demo.domain.model.repository.UserDao;
 public class UserService {
     @Autowired
     UserDao dao;
+    
+    // insert用メソッド
+    public boolean insert(User user) {
+    	// insert実行
+    	int rowNumber = dao.insertOne(user);
+    	
+    	// 判定用変数
+    	boolean result = false;
+    	
+    	if(rowNumber > 0) {
+    		// insert 成功
+    		result = true;
+    	}
+    	
+    	return result;
+    }
 }
