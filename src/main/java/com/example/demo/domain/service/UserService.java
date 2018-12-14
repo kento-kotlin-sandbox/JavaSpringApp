71f14238bcb57b0e528236eb2ba9d4c0cc7f318e
Kento75 <kento2github@gmail.com>
2018-12-15 07:12:45 +0900

    Add ResultSetExtractor

--- src/main/java/com/example/demo/domain/service/UserService.java
+++ src/main/java/com/example/demo/domain/service/UserService.java
@@ -13,7 +13,7 @@ import com.example.demo.domain.model.repository.UserDao;
 @Service
 public class UserService {
     @Autowired
-    @Qualifier("UserDaoJdbcImpl3")
+    @Qualifier("UserDaoJdbcImpl4")
     UserDao dao;
     
     // insert用メソッド
