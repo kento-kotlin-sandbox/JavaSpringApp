28a011a74df0b836b15669205810854d055804a3
Kento75 <kento2github@gmail.com>
2018-12-21 06:20:04 +0900

    Add Exception Handler

--- src/main/java/com/example/demo/trySpring/login/controller/SignupController.java
+++ src/main/java/com/example/demo/trySpring/login/controller/SignupController.java
@@ -4,10 +4,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +38,36 @@ public class SignupController {
 		radio.put("未婚", "false");
 		
 		return radio;
+	}
+	
+	// ExceptionHandler（DB）
+	@ExceptionHandler(DataAccessException.class)
+	public String dataAccessExceptionHandler(DataAccessException e, Model model) {
+		// 例外クラスのメッセージをModelに登録
+		model.addAttribute("error", "内部サーバーエラー（DB）：ExceptionHandler");
+		
+		// 例外クラスのメッセージをModelに登録
+		model.addAttribute("message", "SignupController で DataAccessExceptionが発生しました");
+		
+		// HTTPのエラーコード（500）をModelに登録
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return "error";
+	}
+	
+	// ExceptionHandler（基本）
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e, Model model) {
+		// 例外クラスのメッセージをModelに登録
+		model.addAttribute("error", "内部サーバーエラー： ExceptionHandler");
+		
+		// 例外クラスのメッセージをModelに登録
+		model.addAttribute("message", "SignupController でExceptionが発生しました");
+		
+		// HTTPのエラーコード（500）をModelに登録
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return "error";
 	}
 	
 	// ユーザー登録画面のGET用コントローラー
