d5443e43522d3cd4f315f6ccc92597abb7175121
Kento75 <kento2github@gmail.com>
2018-12-23 20:26:56 +0900

    Add login and logout logic

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -3,17 +3,27 @@ package com.example.demo;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// パスワードエンコーダーBeanの定義
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	// データソース
 	@Autowired
@@ -63,6 +73,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.passwordParameter("password")      // ログインページのパスワード
 			.defaultSuccessUrl("/home", true);  // ログイン成功後の遷移先
 		
+		// ログアウト処理
+		http.logout()
+		    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		    .logoutUrl("/logout")
+		    .logoutSuccessUrl("/login");
+		
 		// CSRF対策を無効に設定（一時的）
 		http.csrf().disable();
 	}
@@ -73,6 +89,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.jdbcAuthentication()
 		    .dataSource(dataSource)
 		    .usersByUsernameQuery(USER_SQL)
-		    .authoritiesByUsernameQuery(ROLE_SQL);
+		    .authoritiesByUsernameQuery(ROLE_SQL)
+		    .passwordEncoder(passwordEncoder());
 	}
 }
